eea62a82e939670d4b2cbdd17db616a389e55fa9
ErickVGusson <92764323+ErickVGusson@users.noreply.github.com>
2023-10-26 16:14:27 -0300

    Inicio da atividade beginner crude

---
+++ config/database.php
@@ -0,0 +1,16 @@
+<?php 
+
+// Usado para conctar no banco
+$host = "localhost";
+$db_name = "php_beginner_crude_level_1";
+$username = "root";
+$password = "";
+
+//Usado para mostrar erros
+try {
+    $con = new PDO("mysql:host={$host}; daname={$db_name}", $username, $password);
+} catch (PDOException $exception) {
+    //echo "Connection errors: " . $exception->getMessage();
+}
+
+?>
